2a9b76e3700dc9f84b1945ef9365e3c5301bddf1
Gabriel Goulart <gabrielgoulartrms@gmail.com>
2025-10-23 17:47:21 -0300

    Adds new endpoint to list all books

--- src/main/java/com/bookread/service/BookService.java
+++ src/main/java/com/bookread/service/BookService.java
@@ -4,9 +4,15 @@ import com.bookread.dto.BookCreateDTO;
 import com.bookread.dto.BookDTO;
 import com.bookread.mapper.BookMapper;
 import com.bookread.model.Book;
+import com.bookread.model.User;
 import com.bookread.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Service
 public class BookService {
@@ -23,5 +29,9 @@ public class BookService {
         book.setAuthor(author);
         Book savedBook = bookRepository.save(book);
         return BookMapper.toDto(savedBook);
+    }
+
+    public List<Book> getBooks() {
+        return bookRepository.findAll();
     }
 }
